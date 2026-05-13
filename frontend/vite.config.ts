6712b14c33a324c8049f2ed57d187a5046f792cc
retfings <1691315371@qq.com>
2026-05-13 21:57:54 +0800

    chore(frontend): add allowed host configuration for Vite dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 80,
+    allowedHosts: ['writer.kangyuetech.cn'],
     proxy: {
       '/api': 'http://localhost:3000',
     },
